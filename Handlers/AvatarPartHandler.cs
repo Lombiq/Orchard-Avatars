043c2bb144de2bc6856a0f355e35be81d35674fb
Piedone <piedone@pyrocenter.hu>
2012-08-16 01:30:17 +0200

    Making lazy-loading part properties better

--- Handlers/AvatarPartHandler.cs
+++ Handlers/AvatarPartHandler.cs
@@ -4,22 +4,20 @@ using Orchard.Core.Common.Models;
 using Orchard.Environment.Extensions;
 using Piedone.Avatars.Models;
 using Piedone.Avatars.Services;
+using Orchard.Environment;
 
 namespace Piedone.Avatars.Handlers
 {
     [OrchardFeature("Piedone.Avatars")]
     public class AvatarPartHandler : ContentHandler
     {
-        private readonly IAvatarsService _avatarsService;
         public AvatarPartHandler(
-            IAvatarsService avatarsService)
+            Work<IAvatarsService> avatarsServiceWork)
         {
-            _avatarsService = avatarsService;
-
-            OnLoaded<AvatarPart>((context, part) =>
+            OnActivated<AvatarPart>((context, part) =>
                 {
                     // Add loaders that will load content just-in-time
-                    part.ImageUrlField.Loader(() => _avatarsService.GetAvatarUrl(part.As<CommonPart>().Owner.Id));
+                    part.ImageUrlField.Loader(() => avatarsServiceWork.Value.GetAvatarUrl(part.As<CommonPart>().Owner.Id));
                 });
         }
     }
--- Handlers/AvatarProfilePartHandler.cs
+++ Handlers/AvatarProfilePartHandler.cs
@@ -3,25 +3,23 @@ using Orchard.Data;
 using Orchard.Environment.Extensions;
 using Piedone.Avatars.Models;
 using Piedone.Avatars.Services;
+using Orchard.Environment;
 
 namespace Piedone.Avatars.Handlers
 {
     [OrchardFeature("Piedone.Avatars")]
     public class AvatarProfilePartHandler : ContentHandler
     {
-        private readonly IAvatarsService _avatarsService;
         public AvatarProfilePartHandler(
             IRepository<AvatarProfilePartRecord> repository,
-            IAvatarsService avatarsService)
+            Work<IAvatarsService> avatarsServiceWork)
         {
             Filters.Add(StorageFilter.For(repository));
 
-            _avatarsService = avatarsService;
-
-            OnLoaded<AvatarProfilePart>((context, part) =>
+            OnActivated<AvatarProfilePart>((context, part) =>
                 {
                     // Add loaders that will load content just-in-time
-                    part.ImageUrlField.Loader(() => _avatarsService.GetAvatarUrl(part.Id));
+                    part.ImageUrlField.Loader(() => avatarsServiceWork.Value.GetAvatarUrl(part.Id));
                 });
         }
     }
