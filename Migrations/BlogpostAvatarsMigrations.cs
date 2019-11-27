9a53bbd664ae11c41bb06e45e791957f87814f3f
Gábor Pór <gabor.por@lombiq.com>
2019-11-27 20:56:34 +0100

    Fixing Blogposts feature name and blog post avatars migrations
    --HG--
    branch : issue/LMBQ-71

--- Migrations/BlogpostAvatarsMigrations.cs
+++ Migrations/BlogpostAvatarsMigrations.cs
@@ -8,8 +8,13 @@ namespace Piedone.Avatars.Migrations
     [OrchardFeature("Piedone.Avatars.Blogpost")]
     public class BlogpostAvatarsMigrations : DataMigrationImpl
     {
+
         public int Create()
         {
+            ContentDefinitionManager.AlterTypeDefinition("BlogPost",
+                cfg => cfg
+                    .WithPart("AvatarPart"));
+
             ContentDefinitionManager.AlterPartDefinition("AvatarPart",
                 builder => builder
                     .WithDescription("Adds Avatars to Blog Posts in Detail view."));
